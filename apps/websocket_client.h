5a112cff6a8dd85888330e23e042a7c5c4bafa80
Mistyrain9302 <vltlr126@naver.com>
2024-06-24 13:04:33 +0900

    99br test2

--- apps/websocket_client.h
+++ apps/websocket_client.h
@@ -1,6 +1,8 @@
 #ifndef WEBSOCKET_CLIENT_H
 #define WEBSOCKET_CLIENT_H
 
+#include <cstddef> // 또는 <stddef.h>
+
 extern "C" {
     void websocket_client_connect();
     void websocket_client_send(const char* data, size_t len, bool is_rx);
